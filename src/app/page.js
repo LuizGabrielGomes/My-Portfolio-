f2fdf911458c9a76e16f2ad633dcf02aaa701f03
Luiz Gabriel Gomes <luizgabrielgomes9686@gmail.com>
2024-11-06 00:47:27 +0000

    update section left

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,28 @@
 import Image from "next/image";
+import photo from "../../public/assets/foto.jpg";
 import { BsLinkedin, BsGithub, BsInstagram } from "react-icons/bs";
+
+import Link from "next/link"
+
 export default function Home() {
-
-
-// to do 
-// arrumar o espacamento entre a borda da div da esquerda e os elementos internos 
-// add o espaco entre a foto e o nome 
-// add o espaco entre o nome e rede sociais 
-// entre a rede social e o paragrafo 
-// add a imagem(minha foto) dentro do circulo 
-// add o link para as redes sociais
 
   return (
     <> 
 
       <div className="p-10 h-lvh bg-indigo-900 flex">
         <div className="bg-blue-950 w-1/5 h-full flex flex-col items-center border-r-4 border-e-emerald-200	">
-          <div className="rounded-full bg-purple-500 w-40 h-40">
-          </div>
+          <Image className="rounded-full bg-purple-500 w-40 h-40 mt-20" alt="gabriel's photos" src={photo} /> 
 
-          <p className="text-purple-50 ">Luiz Gabriel Gomes</p>
+          <p className="text-purple-50 mt-3">Luiz Gabriel Gomes</p>
         
-          <div className="flex gap-3">
-            <BsLinkedin />
-            <BsGithub />
-            <BsInstagram />
+          <div className="flex gap-3 my-6 ">
+            <Link href={"https://www.linkedin.com/in/luiz-gabriel-gomes-171b51264/"} target="_blank"><BsLinkedin /></Link>
+            <Link href={"https://github.com/LuizGabrielGomes"} target="_blank"><BsGithub /></Link>
+            <Link href={"https://www.instagram.com/luizggomes1/"} target="_blank"><BsInstagram /></Link>
           </div>
           
           <div>
-            <p className="text-purple-50">Hi, my name is Luiz Gabriel Gomes and I'm a Front-end Software Engineer. This is my website, be welcome!</p>
+            <p className="text-purple-50 mx-6">Hi, my name is Luiz Gabriel Gomes and I'm a Front-end Software Engineer. This is my website, be welcome!</p>
           </div>
         
         </div> 
